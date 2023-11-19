7c7f252fdb4c87471987887dca88b99e1c20149a
amrsalman <103906208+amrsalman@users.noreply.github.com>
2023-11-19 16:12:24 +0200

    amr 3 push

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -195,5 +195,97 @@ router.route("/user/Verify/:username/client/:Verify").get((req, res) => {
       res.status(500).send("Internal Server Error");
     });
 });
+//
 
+router.route("/user/reset-password").post(async (req, res) => {
+  const { email } = req.body;
+
+  // Check if the user with the provided email exists
+  const existingUser = await user.findOne({
+    where: { email: email },
+  });
+
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ error: "User not found with the provided email." });
+  }
+
+  // Generate a unique token for password reset
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  // Save the token and user information in the database
+  await verify.create({
+    usarname: existingUser.username,
+    crypto: resetToken,
+  });
+
+  // Construct the password reset link
+  const resetLink = `http://localhost:8080/api/v1/user/reset-password/${existingUser.username}/${resetToken}`;
+
+  // Create an HTML template for the email
+  const htmlTemplate = `
+    <div>
+      <p>Click on the link below to reset your password</p>
+      <a href="${resetLink}">Reset Password</a>
+    </div>
+  `;
+
+  // Send the password reset email
+  try {
+    await sendEmail(existingUser.email, "Reset Your Password", htmlTemplate);
+    res.json("We sent you an email with instructions to reset your password.");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router
+  .route("/user/reset-password/:username/:resetToken")
+  .get(async (req, res) => {
+    const { username, resetToken } = req.params;
+
+    // Check if the provided username and reset token match a record in the database
+    const verificationRecord = await verify.findOne({
+      where: { usarname: username, crypto: resetToken },
+    });
+
+    if (verificationRecord) {
+      // If the verification record is found, render a password reset form
+      res.render("PasswordReset", { username, resetToken });
+    } else {
+      res
+        .status(404)
+        .json({ error: "Invalid or expired password reset link." });
+    }
+  })
+  .post(async (req, res) => {
+    const { username, resetToken } = req.params;
+    const { newPassword } = req.body;
+    // Find the user by username
+    const existingUser = await user.findOne({
+      where: { username: username },
+    });
+
+    if (!existingUser) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    // Update the user's password
+    existingUser.password = newPassword;
+    await existingUser.save();
+
+    // Remove the verification record from the database
+    await verify.destroy({
+      where: {
+        usarname: username,
+        crypto: resetToken,
+      },
+    });
+
+    res.json(
+      "Password reset successful. You can now log in with your new password."
+    );
+  });
 module.exports = router;
